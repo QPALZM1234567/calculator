b025485c88a4659d3f2f68812b8954ac137f6b79
Sean <seanjung03@gmail.com>
2022-08-03 12:37:39 -0400

    Fix bug where eventListeners aren't being added

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+//states
+let start = true;
+let number = false;
+let end = false;
+
+
 const btns = document.querySelector(".buttons");
 
+//create buttons
 for (let i = 0; i < 16; i++) {
     let div = document.createElement('div');
     div.style.display = 'flex';
@@ -9,7 +16,7 @@ for (let i = 0; i < 16; i++) {
     p.style.color = 'white';
     if (i < 12 && i % 4 < 3) {
         let temp = Math.floor(i / 4) * 3 + i % 4 + 1;
-        div.classList.add(`${temp}`);
+        div.classList.add(`a${temp}`);
         p.textContent = temp;
     } else if (i === 3) {
         div.classList.add('clear');
@@ -24,8 +31,8 @@ for (let i = 0; i < 16; i++) {
         div.classList.add('plus');
         p.textContent = '+';
     } else if (i === 13) {
-        div.classList.add('0');
-        p.textContent = 0;
+        div.classList.add('a0');
+        p.textContent = '0';
     } else if (i === 14) {
         div.classList.add('minus');
         p.textContent = '-';
@@ -36,4 +43,35 @@ for (let i = 0; i < 16; i++) {
     div.classList.add('button');
     div.appendChild(p);
     btns.appendChild(div);
+}
+
+const text = document.querySelector(".screen p");
+
+//Add functions to the buttons
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    text.textContent = "0";
+    start = true;
+});
+
+for (let i = 0; i < 10; i++) {
+    let temp = document.querySelector(`.a${i}`);
+    console.log(temp);
+    if (i === 0) {
+        temp.addEventListener('click', () => {
+            if (!start) {
+                text.textContent = text.textContent + '0';
+            }
+        });
+    } else {
+        temp.addEventListener('click', () => {
+            if (start) {
+                text.textContent = i;
+                start = false;
+            } else {
+                text.textContent = text.textContent + i;
+            }
+        })
+        
+    }
 }
